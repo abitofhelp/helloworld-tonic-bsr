281e9d7ff36af467b6978b9e3e73b78264b397f1
Mike Gardner <mjgardner@abitofhelp.com>
2023-10-22 01:09:46 -0700

    This is the initial commit of this project.

---
+++ src/gen/mod.rs
@@ -0,0 +1,8 @@
+// @generated
+pub mod helloworld {
+    // @@protoc_insertion_point(attribute:helloworld.v1)
+    pub mod v1 {
+        include!("helloworld.v1.rs");
+        // @@protoc_insertion_point(helloworld.v1)
+    }
+}
