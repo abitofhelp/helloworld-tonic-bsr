ba34e1d8ef6eb7193f936583c838e449dd0fc6ff
Mike Gardner <mjgardner@abitofhelp.com>
2023-10-26 16:56:24 -0700

    Tweaking github release script.

--- build.rs
+++ build.rs
@@ -1,18 +1,19 @@
 use std::error::Error;
 use std::process::{exit, Command};
+use std::env;
+fn main() -> Result<(), Box<dyn Error>> {
+    // We do not want to perform this action for a CICD release build.
+    if Ok("debug".to_owned()) == env::var("PROFILE") {
+        let status = Command::new("buf")
+            .arg("generate")
+            .current_dir(env!("CARGO_MANIFEST_DIR"))
+            .status()
+            .unwrap();
 
-// Uncomment when we need to generate gRPC/Protobuf code.
-// FIXME: Determine how to conditionally compile this code.  We don't want it to be used during a CICD build because we generate the code manually -- not the release build.
-// fn main() -> Result<(), Box<dyn Error>> {
-//     let status = Command::new("buf")
-//         .arg("generate")
-//         .current_dir(env!("CARGO_MANIFEST_DIR"))
-//         .status()
-//         .unwrap();
-//
-//     if !status.success() {
-//         exit(status.code().unwrap_or(-1))
-//     }
-//
-//     Ok(())
-// }
+        if !status.success() {
+            exit(status.code().unwrap_or(-1))
+        }
+    }
+
+    Ok(())
+}
