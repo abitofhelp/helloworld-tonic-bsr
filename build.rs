55610cbcda64c1fa35623ce7f0935f974ba1638c
Mike Gardner <mjgardner@abitofhelp.com>
2023-10-26 16:36:54 -0700

    Tweaking github release script.

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 use std::error::Error;
 use std::process::{exit, Command};
 
+// We manually generate the gRPC/ProtoBuf code, so the following should
+// not be available when performing a CICD release build.  The conditional
+// is determining whether this is a release build.
+#[cfg(not(debug_assertions))]
 fn main() -> Result<(), Box<dyn Error>> {
     let status = Command::new("buf")
         .arg("generate")
